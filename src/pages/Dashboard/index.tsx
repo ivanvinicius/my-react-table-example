bab238ff35e7c1245f09d016e92410964244f70b
ivanvinicius <ivanvinicius88@gmail.com>
2020-12-17 08:53:48 -0300

    add types to hook useTable()

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,68 @@
+import * as React from 'react';
+import { useTable, Column } from 'react-table';
+
+interface IDataProps {
+  [key: string]: string | number;
+}
+
+const Dashboard: React.FC = () => {
+  const data: IDataProps[] = React.useMemo(
+    () => [
+      {
+        col1: 'Hello',
+        col2: 'World',
+      },
+    ],
+    [],
+  );
+
+  const columns = React.useMemo<Column<IDataProps>[]>(
+    () => [
+      {
+        Header: 'Column 1',
+        accessor: 'col1',
+      },
+      {
+        Header: 'Column 2',
+        accessor: 'col2',
+      },
+    ],
+    [],
+  );
+
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+  } = useTable<IDataProps>({ columns, data });
+
+  return (
+    <table {...getTableProps()}>
+      <thead>
+        {headerGroups.map((headerGroup) => (
+          <tr {...headerGroup.getHeaderGroupProps()}>
+            {headerGroup.headers.map((column) => (
+              <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+            ))}
+          </tr>
+        ))}
+      </thead>
+      <tbody {...getTableBodyProps()}>
+        {rows.map((row) => {
+          prepareRow(row);
+          return (
+            <tr {...row.getRowProps()}>
+              {row.cells.map((cell) => {
+                return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>;
+              })}
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+};
+
+export default Dashboard;
